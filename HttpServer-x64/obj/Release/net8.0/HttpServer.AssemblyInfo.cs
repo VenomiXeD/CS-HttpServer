2437a8c28fc57f88a477b9c685d4165474a0f228
VenomiXeD <crafterious@gmail.com>
2024-07-10 06:01:31 +0200

    Readd?

--- HttpServer-x64/obj/Release/net8.0/HttpServer.AssemblyInfo.cs
+++ HttpServer-x64/obj/Release/net8.0/HttpServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HttpServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcbb67ddc6096b94c8785db7a0c36a6ffc114e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777a34c938b1f7932adfbf9ea9a772d2d83948ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("HttpServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HttpServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
